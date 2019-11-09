3af4333dcf3325a7e32adebd75a0b2562fc71f67
李正堂 <zt18356288310@163.com>
2019-11-09 11:34:19 +0800

    feat:

--- src/views/Dashbord/Dashbord.tsx
+++ src/views/Dashbord/Dashbord.tsx
@@ -2,7 +2,15 @@ import React from 'react'
 import './Dashbord.scss'
 
 const Dashbord: React.FC = () => {
-  return <div>Dashbord</div>
+  return (
+    <div>
+      <p>Project starting...</p>
+
+      <p>please wait</p>
+
+      <p>sauron is a good boy! he very very love guoyang!!!</p>
+    </div>
+  )
 }
 
 export default Dashbord
